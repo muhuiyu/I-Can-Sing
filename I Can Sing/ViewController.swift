6a672488deb2ab237ea281c96ec35226c396f226
mlsg-gracemy <grace.yu@monstar-lab.com>
2023-09-21 19:35:12 +0800

    Initial Commit

---
+++ I Can Sing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Can Sing
+//
+//  Created by Grace, Mu-Hui Yu on 9/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Can SingUITests/I_Can_SingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  I_Can_SingUITestsLaunchTests.swift
+//  I Can SingUITests
+//
+//  Created by Grace, Mu-Hui Yu on 9/21/23.
+//
+
+import XCTest
+
+final class I_Can_SingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
